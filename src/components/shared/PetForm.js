c1cbf365efd03a84c0193da870e847bddee0e8fd
Matt Keane <mattpkeane@gmail.com>
2022-07-29 15:23:31 -0500

    added form to PetForm

--- src/components/shared/PetForm.js
+++ src/components/shared/PetForm.js
@@ -1,5 +1,38 @@
+import { 
+    Form,
+    Button, 
+} from 'react-bootstrap'
+
 const PetForm = (props) => {
-    return <p>Here's the pet form</p>
+    return (
+        <Form>
+            <Form.Label htmlFor="name">Name</Form.Label>
+            <Form.Control
+                placeholder="What is your pet's name?"
+                name="name"
+                id="name"
+            />
+            <Form.Label htmlFor="type">Type</Form.Label>
+            <Form.Control
+                placeholder="What kind of pet is this?"
+                name="type"
+                id="type"
+            />
+            <Form.Label htmlFor="age">Age</Form.Label>
+            <Form.Control
+                placeholder="How old is your pet?"
+                type="number"
+                name="age"
+                id="age"
+            />
+            <Form.Check
+                label="Is this pet adoptable?"
+                name="adoptable"
+                defaultChecked={ false  }
+            />
+            <Button type="submit">Submit</Button>
+        </Form>
+    )
 }
 
 export default PetForm
