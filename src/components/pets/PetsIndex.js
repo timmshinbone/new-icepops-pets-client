25ecfbc763ae0300d40b91f007aff3c6f651e9f2
Matt Keane <mattpkeane@gmail.com>
2022-07-29 12:19:49 -0500

    added loading screen

--- src/components/pets/PetsIndex.js
+++ src/components/pets/PetsIndex.js
@@ -1,4 +1,19 @@
+import { useState } from 'react'
+
+import LoadingScreen from '../shared/LoadingScreen'
+
+// PetsIndex should make a request to the api
+// To get all pets
+// Then display them when it gets them
+
 const PetsIndex = (props) => {
+    const [pets, setPets] = useState(null)
+
+    // If pets haven't been loaded yet, show a loading message
+    if (!pets) {
+        return <LoadingScreen />
+    }
+
     return <h1>This is the pets index component</h1>
 }
 
